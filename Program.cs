a176ad6bb885beb9f2171433f823634fa6f64f53
ModeYapu <1207565967@qq.com>
2018-01-11 18:01:05 +0800

    建造者模式

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace CsharpDome
@@ -7,23 +8,95 @@ namespace CsharpDome
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var mobile="17317832jhh559";
-            Regex red=new Regex(@"^[0-9]$");
-            var flag=red.IsMatch(mobile);
-            if(!flag)
-            {
-                Console.WriteLine("这个电话号码不正确");
-            }
-            for(var i=0;i<5;i++)
-            {
-                Console.WriteLine("he"+i);
-            }
+            Director director = new Director();
+            Builder b1 = new ConcreteBuilder1();
+            Builder b2 = new ConcreteBuilder2();
+
+            director.Construct(b1);
+            Product p1 = b1.GetResult();
+            p1.Show();
+
+            director.Construct(b2);
+            Product p2 = b2.GetResult();
+            p2.Show();
             Console.ReadKey();
         }
         private static void Voide()
         {
 
+        }
+    }
+    class Director
+    {
+        public void Construct(Builder builder)
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+        }
+    }
+
+    abstract class Builder
+    {
+        public abstract void BuildPartA();
+        public abstract void BuildPartB();
+        public abstract Product GetResult();
+    }
+
+    class ConcreteBuilder1 : Builder
+    {
+        private Product product = new Product();
+
+        public override void BuildPartA()
+        {
+            product.Add("部件A");
+        }
+
+        public override void BuildPartB()
+        {
+            product.Add("部件B");
+        }
+
+        public override Product GetResult()
+        {
+            return product;
+        }
+    }
+
+    class ConcreteBuilder2 : Builder
+    {
+        private Product product = new Product();
+        public override void BuildPartA()
+        {
+            product.Add("部件X");
+        }
+
+        public override void BuildPartB()
+        {
+            product.Add("部件Y");
+        }
+
+        public override Product GetResult()
+        {
+            return product;
+        }
+    }
+
+    class Product
+    {
+        IList<string> parts = new List<string>();
+
+        public void Add(string part)
+        {
+            parts.Add(part);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("\n产品 创建 ----");
+            foreach (string part in parts)
+            {
+                Console.WriteLine(part);
+            }
         }
     }
 }
